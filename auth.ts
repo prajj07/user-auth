62cda88bda33da2f98b2fa5ff41691ca82f3874c
Prajwal <prajwalvm10@gmail.com>
2024-06-09 22:59:04 +0530

    changes in auth.config.ts

--- auth.ts
+++ auth.ts
@@ -7,6 +7,10 @@ import { db } from "@/lib/db";
 export const { auth, handlers, signIn, signOut } = NextAuth({
 
     callbacks: {
+        async session({ token, session }) {
+            console.log({ session})
+            return session;
+        },
         async jwt({token}){
             console.log({token})
             return token;
