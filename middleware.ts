8a34b58a31d947bf264b57792004516e34145778
Prajwal <prajwalvm10@gmail.com>
2024-06-18 13:23:09 +0530

    axios

--- middleware.ts
+++ middleware.ts
@@ -1,45 +1,65 @@
+import axios from 'axios';
 import NextAuth from 'next-auth';
-
 
 import authConfig from '@/auth.config';
 import {
-    DEFAULT_LOGIN_REDIRECT,
-    apiAuthPrefix,
-    authRoutes,
-    publicRoutes,
-  } from '@/routes';
+  DEFAULT_LOGIN_REDIRECT,
+  apiAuthPrefix,
+  authRoutes,
+  publicRoutes,
+} from '@/routes';
 
- 
-const { auth } = NextAuth(authConfig);
+const axiosInstance = axios.create();
 
-  export default auth ((req) => {       
-    const { nextUrl } = req;                           // Middleware function
+axiosInstance.interceptors.request.use(
+  (config: any, req: any) => {
+    const { nextUrl } = req;
     const isLoggedIn = !!req.auth;
-  
+
     const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
     const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
     const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
-    if (isApiAuthRoute) {
-        return null;
-      }
-    
-      if (isAuthRoute) {
-        if (isLoggedIn) {
-          return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
-        }
-        return null;
-      }
-    
-      if (!isLoggedIn && !isPublicRoute) {
-        return Response.redirect(new URL("/auth/login", nextUrl));
-      }
-    
-      return null;
-    })
-    
+    if (isApiAuthRoute && isLoggedIn) {
+      config.headers.Authorization = `Bearer ${req.auth.token}`;
+    }
 
-// Optionally, don't invoke Middleware on some paths
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+const { auth } = NextAuth(authConfig);
+
+export default auth ((req) => {       
+  const { nextUrl } = req;                           
+  const isLoggedIn =!!req.auth;
+  
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+  if (isApiAuthRoute) {
+    return null;
+  }
+  
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    } else {
+      return Response.redirect(new URL("/auth/login", nextUrl));
+    }
+  }
+  
+  if (!isLoggedIn &&!isPublicRoute) {
+    return Response.redirect(new URL("/auth/login", nextUrl));
+  }
+  
+  return null;
+})
+
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 }
